256786e43504cb4d027ec7aaaea12b4ee2b126ab
Josh Glugatch <joshglugatch@gmail.com>
2020-10-30 13:35:16 -0700

    posts.js creation

--- public/JS/posts.js
+++ public/JS/posts.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 
-var $postContainer = (".postContainer")
+var postContainer = $(".postContainer")
 
 var posts = [];
 
 getPosts();
 
 function initializeRows(){
-    $postContainer.empty();
+    postContainer.empty();
     var rowsToAdd = [];
     for(var i=0; i<posts.length; i++){
         rowsToAdd.push(createNewRow(posts[i]))
     }
-    $postContainer.prepend(rowsToAdd)
+    postContainer.prepend(rowsToAdd)
 }
 
 function getPosts() {
@@ -29,21 +29,15 @@ function createNewRow(post) {
         "<img src='" + post.imageURL+ "'>",
         "<span>",
         post.title,
+        "<br>",
         post.User.name,
         "</span>",
-        "<input type='text' class='edit' style='display: none;'>",
-        "<button class='delete btn btn-danger'>x</button>",
-        "<button class='complete btn btn-primary'>✓</button>",
         "</li>"
       ].join("")
     );
 
-    $newInputRow.find("button.delete").data("id", todo.id);
-    $newInputRow.find("input.edit").css("display", "none");
-    $newInputRow.data("todo", todo);
-    if (todo.complete) {
-      $newInputRow.find("span").css("text-decoration", "line-through");
-    }
+    
+    $newInputRow.data("post", post);
     return $newInputRow;
   }
 
