8bbda0538fc658a5cd8629b8b0704b8d3c26fefe
David Yi <david.yi.0529@gmail.com>
2020-10-31 14:39:32 -0700

    need modal update

--- public/JS/posts.js
+++ public/JS/posts.js
@@ -38,34 +38,30 @@ $(document).ready(function() {
     commentString += '<p>'+ post.Comments[i].comment+ '</p>'
   }
   
-  
-  
-  
-  
-  
       var $newInputRow = $(
         `
-        <div class="col-lg mb-4">
-        <div class="card h-100">
-          <img class="card-img-top" src="${post.imageURL}" alt="">
-          <div class="card-body">
-            <h2 class="card-title">${post.User.username} : ${post.title}</h2>
-            <div class="card-text"> ${commentString} </div>
-          </div>
-          <div class="card-footer">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"></span>
+        <div class="col-md-12">
+            <div class="card h-100 mb-4"alt="">
+            <button type="button" class="btn btn-default" data-toggle="modal" data-target="#exampleModal">
+            <img src="${post.imageURL}" width="500" /></button>
+            <div class="card-body">
+              <h2 class="card-title text-center">${post.User.username} : ${post.title}</h2>
+              <div class="card-text"> ${commentString} </div>
+            </div>
+            <div class="card-footer">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"></span>
+                </div>
+                <textarea class="form-control" aria-label="With textarea"></textarea>
+                <button type="button" class="btn btn-outline-primary">LIKE POST!</button>
               </div>
-              <textarea class="form-control" aria-label="With textarea"></textarea>
-              <button type="button" class="btn btn-outline-primary">LIKE POST!</button>
             </div>
           </div>
-        </div>
       </div>
-  
         `
       )
+      
       $newInputRow.data("post", post);
       return $newInputRow;
     };
