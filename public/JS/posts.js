92d86e9e2003800bf35ea6751af472d74576975d
Josh Glugatch <joshglugatch@gmail.com>
2020-10-31 12:55:34 -0700

    Style post cards/main jumbo

--- public/JS/posts.js
+++ public/JS/posts.js
@@ -1,57 +1,59 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   var postContainer = $(".postContainer")
-  
-  
+
+
   var posts = [];
-  
-  
-  
+
+
+
   getData();
-  
+
   function getData() {
-    $.get("api/posts", function(data){
-        posts = data
-        
-        console.log(data)
-        initializeRows()
+    $.get("api/posts", function (data) {
+      posts = data
+
+      console.log(data)
+      initializeRows()
     })
-   
+
   }
-  
-  function initializeRows(){
-    
+
+  function initializeRows() {
+
     postContainer.empty();
-      var rowsToAdd = [];
-     
-  
-      for(var i=0; i<posts.length; i++){
-          rowsToAdd.push(createNewRow(posts[i]));
-            
-      }
-      console.log(rowsToAdd)
-      postContainer.prepend(rowsToAdd)
-     
+    var rowsToAdd = [];
+
+
+    for (var i = 0; i < posts.length; i++) {
+      rowsToAdd.push(createNewRow(posts[i]));
+
+    }
+    console.log(rowsToAdd)
+    postContainer.prepend(rowsToAdd)
+
   }
-  
+
   function createNewRow(post) {
     console.log(post)
     var commentString = "";
-  for(var i=0; i < post.Comments.length;i++){
-    commentString += '<p class="commentStr">'+post.Comments[i].User.username +":  "+ post.Comments[i].comment+ '</p>'
-  }
-  
-  
-  
-  
-  
-  
-      var $newInputRow = $(
-        `
+    for (var i = 0; i < post.Comments.length; i++) {
+      commentString += '<p class="commentStr">' + post.Comments[i].User.username + ":  " + post.Comments[i].comment + '</p>'
+    }
+
+
+
+
+
+
+    var $newInputRow = $(
+
+      `
+        <div class="row">
         <div class="col-lg mb-4">
         <div class="card h-100">
-          <img class="card-img-top" src="${post.imageURL}" alt="">
+          <img class="card-img-top img-fluid" src="${post.imageURL}" alt="">
           <div class="card-body">
             <h2 class="card-title">${post.User.username} : ${post.title}</h2>
             <div class="card-text"> ${commentString} </div>
@@ -67,11 +69,11 @@ $(document).ready(function() {
           </div>
         </div>
       </div>
-  
+      </div>
         `
-      )
-      $newInputRow.data("post", post);
-      return $newInputRow;
-    };
-      
-  })
+    )
+    $newInputRow.data("post", post);
+    return $newInputRow;
+  };
+
+})
