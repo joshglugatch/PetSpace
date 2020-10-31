b72a13653e0c308f5d75f829009338d664f4fe7f
Josh Glugatch <joshglugatch@gmail.com>
2020-10-31 15:08:58 -0700

    update post styling on main page

--- public/JS/posts.js
+++ public/JS/posts.js
@@ -36,44 +36,44 @@ $(document).ready(function () {
   }
 
   function createNewRow(post) {
-    console.log(post)
-    var commentString = "";
-    for (var i = 0; i < post.Comments.length; i++) {
-      commentString += '<p class="commentStr">' + post.Comments[i].User.username + ":  " + post.Comments[i].comment + '</p>'
-    }
-
-
-
-
-
-
-    var $newInputRow = $(
-
-      `
-        <div class="row">
-        <div class="col-lg mb-4">
-        <div class="card h-100">
-          <img class="card-img-top img-fluid" src="${post.imageURL}" alt="">
-          <div class="card-body">
-            <h2 class="card-title">${post.User.username} : ${post.title}</h2>
-            <div class="card-text"> ${commentString} </div>
-          </div>
-          <div class="card-footer">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"></span>
-              </div>
-              <textarea class="form-control" aria-label="With textarea"></textarea>
-              <button type="button" class="btn btn-outline-primary likeBtn">LIKE POST!</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
+      console.log(post)
+      var commentString = "";
+      for (var i = 0; i < post.Comments.length; i++) {
+        commentString += '<p class="card p-2 m-1">' + post.Comments[i].User.username + ":  " + post.Comments[i].comment + '</p>'
+      }
+  
+  
+  
+  
+  
+  
+      var $newInputRow = $(
         `
-    )
-    $newInputRow.data("post", post);
-    return $newInputRow;
-  };
+            <div class="col-lg mb-4">
+                <div class="card h-100">
+                <img class="card-img-top" src="${post.imageURL}" alt="">
+                <div class="card-body">
+                <h2 class="card-title caption">${post.User.username}:    ${post.title}</h2>
+                <p class="card-text"> ${commentString}</p>
+                </div>
+                <div class="card-footer">
+                <div class="input-group">
+                    
+                    <input type="text" class="form-control" aria-label="With textarea" style="border: orange 2px solid;"></input>
+                    <button type="button" class="btn" style="background-color: orange">Comment</button>
+                </div>
+                </div>
+            </div>
+            </div>
+        `
+
+      )
+
+
+
+
+      $newInputRow.data("post", post);
+      return $newInputRow;
+    };
 
 })
