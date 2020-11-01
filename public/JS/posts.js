a733f68b283bf5c17bc52f84b013eed144dd5ec7
David Yi <david.yi.0529@gmail.com>
2020-10-31 19:32:20 -0700

    update

--- public/JS/posts.js
+++ public/JS/posts.js
@@ -3,10 +3,7 @@ $(document).ready(function () {
 
   var postContainer = $(".postContainer")
 
-
   var posts = [];
-
-
 
   getData();
 
@@ -38,71 +35,34 @@ $(document).ready(function () {
   function createNewRow(post) {
     console.log(post)
     var commentString = "";
-<<<<<<< HEAD
-  for(var i=0; i < post.Comments.length;i++){
-    commentString += '<p>'+ post.Comments[i].comment+ '</p>'
-  }
-  
-      var $newInputRow = $(
-        `
-        <div class="col-md-12">
-            <div class="card h-100 mb-4"alt="">
-            <button type="button" class="btn btn-default" data-toggle="modal" data-target="#exampleModal">
-            <img src="${post.imageURL}" width="500" /></button>
-            <div class="card-body">
-              <h2 class="card-title text-center">${post.User.username} : ${post.title}</h2>
-              <div class="card-text"> ${commentString} </div>
-            </div>
-            <div class="card-footer">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text"></span>
-                </div>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
-                <button type="button" class="btn btn-outline-primary">LIKE POST!</button>
-              </div>
-=======
     for (var i = 0; i < post.Comments.length; i++) {
       commentString += '<p class="commentStr">' + post.Comments[i].User.username + ":  " + post.Comments[i].comment + '</p>'
     }
 
-
-
-
-
-
     var $newInputRow = $(
 
       `
-        <div class="row">
-        <div class="col-lg mb-4">
-        <div class="card h-100">
-          <img class="card-img-top img-fluid" src="${post.imageURL}" alt="">
-          <div class="card-body">
-            <h2 class="card-title">${post.User.username} : ${post.title}</h2>
-            <div class="card-text"> ${commentString} </div>
-          </div>
-          <div class="card-footer">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"></span>
-              </div>
-              <textarea class="form-control" aria-label="With textarea"></textarea>
-              <button type="button" class="btn btn-outline-primary likeBtn">LIKE POST!</button>
->>>>>>> 8abdb6f74ab57e555f155cfb925d38cd4f01ea11
+      <div class="row">
+      <div class="col-md-4 clearfix  d-md-block">
+        <div class="card mb-2">
+          <div class="avatar-wrapper">
+            <img class="card-img-top mx-auto" style="width: 200px;" src="https://github.com/joshglugatch/PetSpace/blob/main/public/images/newLogo_petSpace.png?raw=true" width="500" height="300" alt="" data-toggle="modal" data-target="#basicExampleModal3">
+          <div class="upload-button">
+        <img class="card-img-top img-fluid" src="${post.imageURL}" alt="">
+        <div class="card-body">
+          <h2 class="card-title text-center">${post.User.username} : ${post.title}</h2>
+          <p class="card-text"> ${commentString}</p>
+        </div>
+        <div class="card-footer">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Comment</span>
+            </div>
+            <textarea class="form-control" aria-label="With textarea"></textarea>
+              <button type="button" class="btn btn-outline-primary">LIKE POST!</button>
             </div>
           </div>
-      </div>
-<<<<<<< HEAD
-        `
-      )
-      
-      $newInputRow.data("post", post);
-      return $newInputRow;
-    };
-      
-  })
-=======
+        </div>
       </div>
         `
     )
@@ -111,4 +71,3 @@ $(document).ready(function () {
   };
 
 })
->>>>>>> 8abdb6f74ab57e555f155cfb925d38cd4f01ea11
