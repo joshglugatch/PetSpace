fbfd1ee07f107da8f92bd60d230974ee9354b546
Josh Glugatch <joshglugatch@gmail.com>
2020-10-31 19:17:44 -0700

    fix input grabbing for comments

---
+++ public/JS/createPost.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    
+
+    $(document).on("click", ".commentBtn", function(event) {
+        event.preventDefault();
+        var commentText = $(".commentText" + $(this).data("id")).val();
+        console.log(commentText)
+        var thisID = this.dataset.id;
+        
+        
+
+        
+            $.get("/api/user_data").then(function(data) {
+                var newComment = {
+                    comment: commentText,
+                    PostId: thisID,
+                    UserId: data.id
+                }
+
+                $.post("/api/comment", newComment)
+    
+            .then(function(data) {
+                $(".commentText" + $(this).data("id")).val("");
+                location.reload();
+                console.log(data);
+        });
+
+            });
+          
+        
+        
+
+    })
+    
+});
