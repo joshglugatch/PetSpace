8420617e647f0268fae861513815d9ed24067da9
Josh Glugatch <joshglugatch@gmail.com>
2020-10-31 21:18:20 -0700

    implement creating new posts

--- public/JS/images.js
+++ public/JS/images.js
@@ -6,6 +6,10 @@ var CLOUDINARY_UPLOAD_PRESET = 'hcqzg87z'
 var imgPreview = document.getElementById('img-preview');
 var fileUpload = document.getElementById('file-upload');
 
+
+var createBtn = document.querySelector('.btn-deep-orange');
+var link;
+
 fileUpload.addEventListener('change', function(event){
 var file = event.target.files[0];
 var formData = new FormData();
@@ -23,23 +27,38 @@ formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)
     }).then(function(res){
     console.log(res)
     imgPreview.src = res.data.secure_url      
+    link = res.data.secure_url      
+
+    createBtn.addEventListener("click", function(event){
+
+        axios.get('/api/user_data').then(data => {
+            console.log(data)
+            var thisID = data.data.id
+            var animal = document.getElementById('inputGroupSelect04').value;
+            var caption = document.getElementById('orangeForm-name').value;
+
             axios({
                 url: "/api/posts",
                 method: "POST",
                 data: {
-                    title: "imgtest",
-                    animal: "dog",
-                    UserId: 1,
+                    title: caption,
+                    animal: animal,
+                    UserId: thisID,
                     imageURL: res.data.secure_url
                 }  
             }).then(function(res){
                 console.log(res)
+                location.reload();
             }).catch(function(err){
                 console.log(err)
-            });           
+            });   
+        });       
+    })
+    
+         
 
     }).catch(function(err){
     console.log(err)
     });
-
+    
 });
