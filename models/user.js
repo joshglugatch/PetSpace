54c2b1a5e766e813c3aa6b896a4296e4ed18da5f
Josh Glugatch <joshglugatch@gmail.com>
2020-11-02 23:12:39 -0800

    password validation and bcrypt

--- models/user.js
+++ models/user.js
@@ -1,13 +1,17 @@
+var bcrypt = require("bcryptjs");
+
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
       username: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true
     },
 
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {len:[8,30]}
     }
     });
   
@@ -17,5 +21,13 @@ module.exports = function(sequelize, DataTypes) {
       User.hasMany(models.Comment) 
     };
   
-    return User;
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
   };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  return User;
+};
